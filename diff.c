696f24340b6136eb4282dbf07483b9b6dbf79dd6
Ayushman Gupta <ayushmang3112@gmail.com>
2023-02-19 02:25:14 +0530

    multiply initial commit

---
+++ diff.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main() {
+    int a,b;
+    printf("Enter a");
+    scanf("%d",&a);
+
+    printf("Enter b");
+    scanf("%d", &b);
+
+    printf("Sum is :%d" , a-b);
+
+    return 0;
+}
